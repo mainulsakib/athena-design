cd55eedc60b224703181c7f86e8df5ae242e85e3
mainulsakib <losersakib@gmail.com>
2020-11-23 12:16:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+import "./App.css";
+import Achievement from './Components/Achievement/Achievement';
+import Banner from './Components/Banner/Banner';
+import ChooseTeam from "./Components/ChooseTeam/ChooseTeam";
+import Header from "./Components/Header/Header";
+import Project from './Components/Project/Project';
+import Service from './Components/Services/Service';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        
+        <div>
+            <Router>
+                <Switch>
+                    <Route>
+                        <Header></Header>
+                        <Banner></Banner>
+                        <Service></Service>
+                        <Project></Project>
+                        <Achievement></Achievement>
+                        <ChooseTeam></ChooseTeam>
+                        
+                        
+                    </Route>
+                    <Route>
+                        
+                    </Route>
+                </Switch>
+            </Router>
+            
+        </div>
+    );
 }
 
 export default App;
